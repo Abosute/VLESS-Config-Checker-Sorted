00325d745a4a2eb6db70a56d940d7f39d5ec6345
Abosut-ThinkPad <dibirsaba@gmail.com>
2026-03-03 01:57:21 +0300

    added funcionally to GItHUbClient

--- github-client.py
+++ github-client.py
@@ -12,6 +12,9 @@ class LogMessage(Enum):
     UNEXPECTED = "Unexpected error {}"
     HTTPERROR = 'HTTPStatusError {}'
     EMPTY_FILES = 'Empty files'
+    INCORRECT_TYPE = 'Inccorect type'
+
+
 
 class GitHubClient:
     def __init__(self, url: str):
@@ -37,21 +40,28 @@ class GitHubClient:
             self._api_url = ""
             logger.error(LogMessage.INVALID_LINK.value)
 
-    async def _get_contents(self) -> Optional[list[dict]]:
+    async def _get_contents(self, client: httpx.AsyncClient) -> Optional[list[dict]]:
         try:
-            async with httpx.AsyncClient(timeout=1.0) as client:
-                response = await client.get(self.api_url)
+            response = await client.get(self.api_url)
 
-                response.raise_for_status()
+            response.raise_for_status()
 
-                return response.json()
+            return response.json()
         except HTTPStatusError as e:
             logger.error(LogMessage.HTTPERROR.value.format(e))
         except Exception as e:
             logger.error(LogMessage.UNEXPECTED.value.format(e))
 
-    async def get_sorted_by(self, *, type_file: str="file", in_name: str='') -> Optional[list]:
-        files = await self._get_contents()
+    async def get_sorted_by(self, *, type_file: str | list="file", name_file: str | list='', client: Optional[httpx.AsyncClient]=None) -> list[str]:
+        if not client:
+            async with httpx.AsyncClient() as independent_client:
+                files = await self._get_contents(independent_client)
+        else:
+            files = await self._get_contents(client=client)
+
+        if not isinstance(type_file, (str, list)) or not isinstance(name_file, (str, list)):
+            logger.error(LogMessage.INCORRECT_TYPE.value)
+            return []
 
         if not files:
             logger.error(LogMessage.EMPTY_FILES.value)
@@ -60,18 +70,55 @@ class GitHubClient:
         sorted_files = []
 
         for file in files:
-            sorted_by_type = type_file == file['type']
-            sorted_by_name = in_name in file['name']
+            sorted_by_type: bool = type_file == file['type'] if isinstance(type_file, str) else file['type'] in type_file
+            sorted_by_name: bool = name_file in file['name'] if isinstance(name_file, str) else file['name'] in name_file #if the name is a str, it is checked for inclusion
+
             if sorted_by_type and sorted_by_name:
                 sorted_files.append(file['download_url'])
 
         return sorted_files
+    
+    async def download_contents(self, contents: list[str]) -> list[str]:
+        if not isinstance(contents, list):
+            logger.error(LogMessage.INCORRECT_TYPE.value)
+            return []
+        
+        if not contents:
+            logger.error(LogMessage.EMPTY_FILES.value)
+            return []
+        
+        contents_list = []
+
+
+'''        async with httpx.AsyncClient() as client:
+            async for link in contents:
+                try:
+                    response = await client.get(link)
+
+                    response.raise_for_status()
+
+                    test = response.text
+
+                    contents_list.append(test)
+                except HTTPStatusError:
+                    pass
+                except Exception:
+                    pass
+
+            return contents_list'''
+        
+
+
+
 
 client = GitHubClient("https://github.com/Magerko/universal-media-downloader")
 
 async def main():
-    ddd = await client.get_sorted_by(in_name=".txt")
+    ddd = await client.get_sorted_by(name_file='.txt', type_file=['file'])
     print(ddd)
 
+    ddddd = await client.download_contents(ddd)
+
+    print(ddddd)
 
 asyncio.run(main())
